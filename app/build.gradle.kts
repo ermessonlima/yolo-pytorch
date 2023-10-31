6b9c34269cef3d73176edfb5f4cd86389c790a89
Ermesson Lima <ermessonlima@mac-ermes.local>
2023-10-30 23:11:45 -0300

    feat: test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,12 +26,18 @@ android {
             )
         }
     }
+
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        mlModelBinding = true
     }
 }
 
@@ -41,6 +47,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite:2.4.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.4.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/yolo/MainActivity.kt
+++ app/src/main/java/com/example/yolo/MainActivity.kt
@@ -3,54 +3,104 @@ package com.example.yolo
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
-import org.pytorch.IValue
-import org.pytorch.Module
-import org.pytorch.torchvision.TensorImageUtils
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yolo.ml.BestFp16
+import com.example.yolo.tflite.Classifier
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val MINIMUM_CONFIDENCE_TF_OD_API = 0.3f
+    }
+
+     lateinit var model: BestFp16
+     lateinit var image: Bitmap
+     lateinit var button: Button
+     lateinit var imageView: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        try {
-            val bitmap = BitmapFactory.decodeStream(assets.open("image.png"));
-            val moduleFilePath = assetFilePath(this, "best.torchscript.ptl")
-            val module = Module.load(moduleFilePath)
 
-            val expectedWidth = 320
-            val expectedHeight = 320
+         image = BitmapFactory.decodeStream(assets.open("testeimage.jpg"));
 
-            val resizedBitmap =
-                Bitmap.createScaledBitmap(bitmap, expectedWidth, expectedHeight, false)
-            val inputTensor = TensorImageUtils.bitmapToFloat32Tensor(
-                resizedBitmap,
-                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB
-            )
+        button = findViewById(R.id.buttonId)
+        imageView = findViewById(R.id.imageView)
 
-//            val (x, boxes, scores) = module.forward(IValue.from(inputTensor)).toTuple()
-//
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                model = BestFp16.newInstance(applicationContext)
+                image = Bitmap.createScaledBitmap(image,640,640, false)
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 640, 640, 3), DataType.FLOAT32)
+                 val byteBuffer = ByteBuffer.allocateDirect(4 * 640 * 640 * 3 );
+                byteBuffer.order(ByteOrder.nativeOrder())
+
+                val intValues = IntArray(640 * 640)
+                image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+                var pixel = 0
+                for (i in 0 until 640) {
+                    for (j in 0 until 640) {
+                        val `val` = intValues[pixel++] // RGB
+                        byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 1))
+                        byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 1))
+                        byteBuffer.putFloat((`val` and 0xFF) * (1f / 1))
+                    }
+                }
+                inputFeature0.loadBuffer(byteBuffer);
+
+                val outputs = model.process(inputFeature0)
+
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+                val confidences = outputFeature0.getFloatArray()
 
 
+                Log.d("dddddddddd", "eeee" + confidences)
+                model.close()
 
 
+            }
+        })
 
-            val imageView = findViewById<ImageView>(R.id.imageView)
-            imageView.setImageBitmap(bitmap)
 
-            Log.e("Successzz", "Success")
-        } catch (e: IOException) {
-            Log.e("Error", "Error reading assets", e)
-            finish()
+        fun handleResult(bitmap: Bitmap, results: List<Classifier.Recognition>) {
+            val canvas = Canvas(bitmap)
+            val paint = Paint().apply {
+                color = Color.RED
+                style = Paint.Style.STROKE
+                strokeWidth = 2.0f
+            }
+
+            for (result in results) {
+                val location = result.location
+                if (location != null && result.confidence >= MINIMUM_CONFIDENCE_TF_OD_API) {
+                    canvas.drawRect(location, paint)
+                }
+            }
+
+            imageView.setImageBitmap(image)
         }
+
     }
+
+
+
+
 
 
     @Throws(IOException::class)
